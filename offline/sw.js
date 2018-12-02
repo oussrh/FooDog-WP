9e54fcb2de71be76fe0dd067c75e22ce205c48ab
oussrh <ouss_rh@hotmail.com>
2018-12-02 11:00:56 +0100

    sw9

--- offline/sw.js
+++ offline/sw.js
@@ -1,36 +1,26 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/offline/sw.js', {
-      scope: '/offline/'
+let cacheVersion = "foodog5";
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheVersion).then(cache => {
+      return cache.addAll([
+        `offline/`,
+        `offline/index.html`
+      ])
+          .then(() => self.skipWaiting());
     })
-    .then(function () {
-      console.log("Service Worker Registered");
-      self.addEventListener('install', e => {
-        e.waitUntil(
-          caches.open('foodog1').then(cache => {
-            return cache.addAll([
-                `offline/`,
-                `offline/index.html`
-              ])
-              .then(() => self.skipWaiting());
-          })
-        );
-      });
+  );
+});
 
-      self.addEventListener('activate', event => {
-        event.waitUntil(self.clients.claim());
-      });
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
 
-      self.addEventListener('fetch', event => {
-        event.respondWith(
-          caches.open('foodog1')
-          .then(cache => cache.match(event.request, {
-            ignoreSearch: true
-          }))
-          .then(response => {
-            return response || fetch(event.request);
-          })
-        );
-      })
-    });
-}
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheVersion)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+})
