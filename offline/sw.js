c9af6983959840a2a4d736a5516e082b15f7d87e
oussrh <ouss_rh@hotmail.com>
2018-12-02 11:50:22 +0100

    sw15

--- offline/sw.js
+++ offline/sw.js
@@ -1,5 +1,5 @@
-const version = "0.2.11";
-const cacheName = `foodog-${version}`;
+
+const cacheName = `FoodDog`;
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
