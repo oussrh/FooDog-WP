f465d91d8ed6aedd95893aeb957edb0b857f54e0
oussrh <ouss_rh@hotmail.com>
2018-12-02 11:10:52 +0100

    sw10

--- offline/sw.js
+++ offline/sw.js
@@ -1,26 +1,34 @@
-let cacheVersion = "foodog5";
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheVersion).then(cache => {
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
-        `offline/`,
-        `offline/index.html`
-      ])
-          .then(() => self.skipWaiting());
+        '/offline/',
+        '/offline/index.html'
+      ]);
     })
   );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        alert('je sais pas');
+      });
+    }
+  }));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheVersion)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
-})
