7d688377127e9d030a51bc48304178cb3b136cc6
oussrh <ouss_rh@hotmail.com>
2018-12-02 14:52:31 +0100

    sw25

--- offline/sw.js
+++ offline/sw.js
@@ -1,31 +1,15 @@
-/*
- Copyright 2015 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
 
 'use strict';
 
-// Incrementing CACHE_VERSION will kick off the install event and force previously cached
-// resources to be cached again.
 const CACHE_VERSION = 2;
 let CURRENT_CACHES = {
   offline: 'offline-v' + CACHE_VERSION
 };
-const OFFLINE_URL = '/FooDog-WP/offline/index.html';
+const OFFLINE_URL = '/FooDog-WP/offline/';
 
 function createCacheBustedRequest(url) {
   let request = new Request(url, {cache: 'reload'});
-  // See https://fetch.spec.whatwg.org/#concept-request-mode
-  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see
-  // if the cache: 'reload' option had any effect.
+
   if ('cache' in request) {
     return request;
   }
