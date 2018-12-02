d9b3a6dc0f96085aeb5e54f9f1d9420fd30d8750
oussrh <ouss_rh@hotmail.com>
2018-12-02 10:11:29 +0100

    sw3

--- JS/sw.js
+++ JS/sw.js
@@ -1,6 +1,39 @@
+const version = "0.6.11";
+const cacheName = `Foodog-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        `/`,
+        `/index.html`,
+        `/CSS/style.css`,
+        `/CSS/index.css`,
+        `/CSS/footer.css`,
+        `/CSS/categorie.css`,
+        `/CSS/article.css`,
+        `/JS/article.js`,
+        `/JS/categorie.js`,
+        `/JS/foodogwebc.js`,
+        `/JS/footer.js`,
+        `/JS/header.js`,
+        `/JS/index.js`,
+        `/img/nophoto.svg`
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
 
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('/sw.js')
-           .then(function() { console.log("Service Worker Registered"); });
-}
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+})
