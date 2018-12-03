f452423cd485d0d14351c092e1d4c3d77874fc8b
oussrh <ouss_rh@hotmail.com>
2018-12-03 09:55:11 +0100

    pagination

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,21 @@
 xhr = new XMLHttpRequest();
-xhr.open("GET", "https://foodog.herokuapp.com/articles", true);
+xhr2 = new XMLHttpRequest();
 
+let pageUrl = new URLSearchParams(window.location.search);
+pageUrl = pageUrl.get('page');
+
+url = `https://foodog.herokuapp.com/articles?page=${pageUrl}`;
+
+xhr.open('GET', url, true);
+xhr2.open('GET', url, true);
 
 
 class Articles extends HTMLElement {
         constructor() {
                 super();
-                let shadow = this.attachShadow({ mode: 'open' });
+                let shadow = this.attachShadow({
+                        mode: 'open'
+                });
                 let main = document.createElement('main');
                 let style = document.createElement('link');
                 style.setAttribute('rel', 'stylesheet');
@@ -24,7 +33,7 @@ class Articles extends HTMLElement {
 
 
                 xhr.onload = function () {
-                        
+
                         if (xhr.status === 200 && xhr.readyState === 4) {
                                 let response = JSON.parse(xhr.responseText)
 
@@ -36,14 +45,13 @@ class Articles extends HTMLElement {
                                         for (let x = 0; x < response.docs[i].tagForArticle.length; x++) {
                                                 if (x < (response.docs[i].tagForArticle.length - 1)) {
                                                         tagForArticle += `<a href="#">${response.docs[i].tagForArticle[x]}</a>`
-                                                }
-                                                else {
+                                                } else {
                                                         tagForArticle += `<a href="#" style="border-right: 1px solid rgb(238, 238, 238)">${response.docs[i].tagForArticle[x]}</a>`
                                                 }
 
                                         }
                                         let url = response.docs[i].imgUrl.replace(/^http:\/\//i, 'https://');
-                                        div.innerHTML = /*html*/`  
+                                        div.innerHTML = /*html*/ `  
                 <div class="article">
                         <a class ="img-hover"href="article.html?id=${(response.docs[i]._id)}"><img src="${url}" alt="${response.docs[i].title}" class="image1"></a>
                         <div class="content">
@@ -64,4 +72,42 @@ class Articles extends HTMLElement {
         }
 }
 xhr.send();
+
+class Pagination extends HTMLElement {
+        constructor() {
+                super();
+
+                let section = document.createElement('section');
+                section.setAttribute('id', 'pagination');
+                this.appendChild(section);
+
+                xhr2.onload = function () {
+
+                        if (xhr2.readyState === 4 && xhr2.status === 200) {
+                                let data = JSON.parse(xhr2.responseText);
+
+                                for (let i = 1; i <= data.pages; i++) {
+                                        let pageLink = document.createElement('a');
+                                        pageLink.setAttribute("class", "page");
+                                        pageLink.setAttribute("href", `?page=${i}`);
+
+                                        pageLink.innerHTML = `${i}`;
+                                        section.appendChild(pageLink);
+
+                                }
+
+
+
+                        } else {
+
+                                console.error(xhr.statusText);
+
+                        }
+                }
+
+        }
+}
+xhr2.send();
+
 customElements.define('main-articles', Articles);
+customElements.define('pagination-comp', Pagination);
